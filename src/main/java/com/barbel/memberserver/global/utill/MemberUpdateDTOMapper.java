8b0dc7e03c6552efcab29d425ab65a5b352f572e
Identityless <rlandwnd555@naver.com>
2023-02-14 02:41:29 +0900

    fix : builder 사용하는 로직 오류 수정

--- src/main/java/com/barbel/memberserver/global/utill/MemberUpdateDTOMapper.java
+++ src/main/java/com/barbel/memberserver/global/utill/MemberUpdateDTOMapper.java
@@ -4,15 +4,14 @@ import com.barbel.memberserver.domain.member.dto.MemberUpdateRequest;
 import com.barbel.memberserver.domain.member.entity.Member;
 
 public class MemberUpdateDTOMapper {
-  public static Member memberUpdateRequestToMember(MemberUpdateRequest memberUpdateRequest) {
-    return Member.builder()
-            .nickname(memberUpdateRequest.getNickname())
-            .address(memberUpdateRequest.getAddress())
-            .addressDetail(memberUpdateRequest.getAddressDetail())
-            .introduce(memberUpdateRequest.getIntroduce())
-            .majors(memberUpdateRequest.getMajors())
-            .interests(memberUpdateRequest.getInterests())
-            .role(memberUpdateRequest.getRole())
-            .build();
+  public static Member memberUpdateRequestToMember(MemberUpdateRequest memberUpdateRequest, Member member) {
+    member.setNickname(memberUpdateRequest.getNickname());
+    member.setAddress(memberUpdateRequest.getAddress());
+    member.setAddressDetail(memberUpdateRequest.getAddressDetail());
+    member.setIntroduce(memberUpdateRequest.getIntroduce());
+    member.setMajors(memberUpdateRequest.getMajors());
+    member.setInterests(memberUpdateRequest.getInterests());
+    member.setRole(memberUpdateRequest.getRole());
+    return member;
   }
 }
