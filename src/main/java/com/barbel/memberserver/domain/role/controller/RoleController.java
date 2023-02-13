e12b08a188ac2d5ca964cb619f2196bb8ce69a49
Identityless <rlandwnd555@naver.com>
2023-02-14 03:18:12 +0900

    feat : RoleController 구현

---
+++ src/main/java/com/barbel/memberserver/domain/role/controller/RoleController.java
@@ -0,0 +1,44 @@
+package com.barbel.memberserver.domain.role.controller;
+
+import com.barbel.memberserver.domain.role.document.Role;
+import com.barbel.memberserver.domain.role.service.RoleService;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static com.barbel.memberserver.domain.role.controller.RoleController.MEMBER_API_URI;
+
+@RestController
+@RequiredArgsConstructor(access = AccessLevel.PROTECTED)
+@Slf4j
+@RequestMapping(MEMBER_API_URI)
+public class RoleController {
+  public static final String MEMBER_API_URI = "/api/role";
+  private final RoleService roleService;
+
+  @PostMapping("/add")
+  public String addRole(@RequestBody Role role) {
+    if(roleService.saveRole(role)) {
+      return "역할 추가 완료";
+    } else {
+      return "역할 추가 실패";
+    }
+  }
+
+  @PostMapping("/delete")
+  public String deleteRole(@RequestBody Role role) {
+    if(roleService.deleteRole(role)) {
+      return "역할 삭제 완료";
+    } else {
+      return "역할 삭제 실패";
+    }
+  }
+
+  @GetMapping("/list")
+  public List<Role> getRoleList() {
+    return roleService.findAll();
+  }
+}
