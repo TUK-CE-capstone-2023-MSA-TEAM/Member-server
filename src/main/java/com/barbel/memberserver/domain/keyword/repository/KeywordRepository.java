1ea847010c64d5fbb215b57aa616cb9d63f8d603
Identityless <rlandwnd555@naver.com>
2023-02-14 03:06:15 +0900

    feat : KeywordRepository 추가

---
+++ src/main/java/com/barbel/memberserver/domain/keyword/repository/KeywordRepository.java
@@ -0,0 +1,9 @@
+package com.barbel.memberserver.domain.keyword.repository;
+
+import com.barbel.memberserver.domain.keyword.document.Keyword;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface KeywordRepository extends MongoRepository<Keyword, String> {
+}
