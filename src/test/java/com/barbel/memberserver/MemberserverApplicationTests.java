048b4046b4c84ddd02b398e9a41d9db3aaa0f0b9
Identityless <rlandwnd555@naver.com>
2023-02-13 22:37:54 +0900

    test : 회원 가입 및 조회 API 테스트 추가

--- src/test/java/com/barbel/memberserver/MemberserverApplicationTests.java
+++ src/test/java/com/barbel/memberserver/MemberserverApplicationTests.java
@@ -1,13 +1,40 @@
 package com.barbel.memberserver;
 
+import com.barbel.memberserver.domain.member.entity.Member;
+import com.barbel.memberserver.domain.member.repository.MemberRepository;
+import com.barbel.memberserver.domain.member.service.MemberService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
 class MemberserverApplicationTests {
 
+  @Autowired
+  private MemberService memberService;
   @Test
   void contextLoads() {
+  }
+
+  @Test
+  void dbConnectionTest() {
+    Member member = Member.builder().email("rlandwnd555@naver.com")
+        .age(25).name("김우중").password("password").sex("남자").nickname("로니콜먼")
+        .address("서울특별시").addressDetail("강남구").role("고수").interests(List.of("운동", "독서")).majors(List.of("컴퓨터공학")).build();
+    try {
+      memberService.saveMember(member);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    Member member1 = memberService.findMemberByEmail("rlandwnd555@naver.com");
+    System.out.println(member1);
+    assertThrows(IllegalArgumentException.class, () -> {
+      memberService.findMemberByEmail("asdfasdf@gmail.com");
+    });
   }
 
 }
