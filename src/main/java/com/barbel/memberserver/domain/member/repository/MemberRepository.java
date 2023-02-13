64e358eb5496b7f1c5fe851c97d7e38fb3ee695e
Identityless <rlandwnd555@naver.com>
2023-02-13 22:27:47 +0900

    feat : MemberRepository 구현

---
+++ src/main/java/com/barbel/memberserver/domain/member/repository/MemberRepository.java
@@ -0,0 +1,17 @@
+package com.barbel.memberserver.domain.member.repository;
+
+import com.barbel.memberserver.domain.member.entity.Member;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface MemberRepository extends MongoRepository<Member, String> {
+  List<Member> findAll();
+  Optional<Member> findAllByMajorsContains(String interest);
+  Optional<Member> findAllByInterestsContains(String major);
+  Optional<Member> findByEmail(String email);
+  void deleteByEmail(String email);
+}
