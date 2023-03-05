23f6dfc23191a0c05d1e562b6ef0e01761d0206f
WooJoongKim <rlandwnd555@naver.com>
2023-03-05 22:12:34 +0900

    feat : 결과 응답 객체 구현

--- src/main/java/com/barbel/memberserver/global/result/ResultResponse.java
+++ src/main/java/com/barbel/memberserver/global/result/ResultResponse.java
@@ -4,9 +4,22 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class ResultResponse {
-  private int code;
+  private String code;
   private String message;
   private Object data;
+
+  public static ResultResponse of(ResultCode resultCode, Object data) {
+    return new ResultResponse(resultCode, data);
+  }
+
+  public static ResultResponse of(ResultCode resultCode) {
+    return new ResultResponse(resultCode, "");
+  }
+
+  private ResultResponse(ResultCode resultCode, Object data) {
+    this.code = resultCode.getCode();
+    this.message = resultCode.getMessage();
+    this.data = data;
+  }
 }
