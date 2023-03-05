4a953d2febf7ee5c13157aa7134152936ca2d3ab
WooJoongKim <rlandwnd555@naver.com>
2023-03-05 22:13:58 +0900

    feat : 비즈니스 예외 추가

--- src/main/java/com/barbel/memberserver/global/error/exception/BusinessException.java
+++ src/main/java/com/barbel/memberserver/global/error/exception/BusinessException.java
@@ -1,4 +1,14 @@
 package com.barbel.memberserver.global.error.exception;
 
-public class BusinessException {
+import com.barbel.memberserver.global.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException{
+  private final ErrorCode errorCode;
+
+  public BusinessException(ErrorCode errorCode) {
+    super(errorCode.getMessage());
+    this.errorCode = errorCode;
+  }
 }
