5fcc57a8047dea3c1be7515e26710698a73bfc6d
WooJoongKim <rlandwnd555@naver.com>
2023-03-05 23:43:33 +0900

    feat : 역할 업데이트 DTO 추가

---
+++ src/main/java/com/barbel/memberserver/domain/role/dto/RoleUpdateRequest.java
@@ -0,0 +1,19 @@
+package com.barbel.memberserver.domain.role.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class RoleUpdateRequest {
+  private String rolePre;
+  private String rolePost;
+  private String description;
+}
