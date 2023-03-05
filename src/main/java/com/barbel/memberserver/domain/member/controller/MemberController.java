709571eae19e5231ad51e94f8aea320fc5c6128d
WooJoongKim <rlandwnd555@naver.com>
2023-03-05 23:32:56 +0900

    fix : ResponseEntity 반환하도록 수정

--- src/main/java/com/barbel/memberserver/domain/member/controller/MemberController.java
+++ src/main/java/com/barbel/memberserver/domain/member/controller/MemberController.java
@@ -7,10 +7,14 @@ import com.barbel.memberserver.domain.member.dto.MemberLoginRequest;
 import com.barbel.memberserver.domain.member.dto.MemberRegistrationRequest;
 import com.barbel.memberserver.domain.member.document.Member;
 import com.barbel.memberserver.domain.member.service.MemberService;
+import com.barbel.memberserver.global.result.ResultCode;
+import com.barbel.memberserver.global.result.ResultResponse;
 import com.barbel.memberserver.global.utill.MemberUtil;
+import javax.validation.Valid;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +22,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
@@ -28,45 +31,48 @@ public class MemberController {
   public static final String MEMBER_API_URI = "/api/member";
   private final MemberService memberService;
 
+  // TODO: 회원가입, 로그인 빼고 로그인 필요.
   @PostMapping("/signup")
-  public String registration(@RequestBody MemberRegistrationRequest memberRegistrationRequest) {
+  public ResponseEntity<ResultResponse> registration(
+      @RequestBody @Valid MemberRegistrationRequest memberRegistrationRequest) {
     log.info(memberRegistrationRequest.toString());
     Member member = MemberUtil.memberRegistrationRequestToMember(memberRegistrationRequest);
     memberService.saveMember(member);
-    return "Registration Success";
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.MEMBER_REGISTRATION_SUCCESS));
   }
 
   @PostMapping("/signin")
-  public String Login(@RequestBody MemberLoginRequest memberLoginRequest) {
+  public ResponseEntity<ResultResponse> Login(@RequestBody MemberLoginRequest memberLoginRequest) {
+    //TODO: 로그인 처리 로직 구현
     log.info(memberLoginRequest.toString());
-    return "Login Success";
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.MEMBER_LOGIN_SUCCESS));
   }
 
   @GetMapping("/logout")
-  public String Logout() {
-    return "로그아웃 완료";
+  public ResponseEntity<ResultResponse> Logout() {
+    //TODO: 로그아웃 처리 로직 구현
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.MEMBER_LOGOUT_SUCCESS));
   }
 
   @GetMapping("/info")
-  public List<Member> getMemberInfo() {
-    List<Member> memberList = memberService.findAll();
+  public ResponseEntity<ResultResponse> getMemberInfo() {
+    String dummyEmail = "";
+    Member memberList = memberService.findMemberByEmail(dummyEmail);
     log.info(memberList.toString());
-    return memberList;
-  }
-
-  @GetMapping("/duplicated/{email}")
-  public Boolean isDuplicatedEmail(@PathVariable String email) {
-    if(memberService.isDuplicatedEmail(email)) {
-      return false;
-    }
-    else {
-      return true;
-    }
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.MEMBER_FIND_SUCCESS, memberList));
   }
 
   @GetMapping("/delete/{email}")
-  public String deleteMember(@PathVariable String email) {
+  public ResponseEntity<ResultResponse> deleteMember(@PathVariable String email) {
+    //TODO: 회원 탈퇴 DTO 정의한 다음 비밀번호 입력받도록 수정.
     memberService.deleteMemberByEmail(email);
-    return "삭제 완료";
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.MEMBER_DELETE_SUCCESS));
   }
+
+  @GetMapping("/list")
+  public ResponseEntity<ResultResponse> getMemberList() {
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.MEMBER_LIST_REQUEST_SUCCESS, memberService.findAll()));
+  }
+
+  // TODO: 특정 특기나 관심사 등으로 검색하는 기능 구현. (메서드 2개 필요)
 }
--- src/main/java/com/barbel/memberserver/domain/role/controller/RoleController.java
+++ src/main/java/com/barbel/memberserver/domain/role/controller/RoleController.java
@@ -3,10 +3,14 @@ package com.barbel.memberserver.domain.role.controller;
 import com.barbel.memberserver.domain.role.document.Role;
 import com.barbel.memberserver.domain.role.dto.RoleDeleteRequest;
 import com.barbel.memberserver.domain.role.dto.RoleRegistrationRequest;
+import com.barbel.memberserver.domain.role.dto.RoleUpdateRequest;
 import com.barbel.memberserver.domain.role.service.RoleService;
+import com.barbel.memberserver.global.result.ResultCode;
+import com.barbel.memberserver.global.result.ResultResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -22,25 +26,29 @@ public class RoleController {
   private final RoleService roleService;
 
   @PostMapping("/add")
-  public String addRole(@RequestBody RoleRegistrationRequest roleRegistrationRequest) {
-    if(roleService.saveRole(roleRegistrationRequest)) {
-      return "역할 추가 완료";
-    } else {
-      return "역할 추가 실패";
-    }
+  // TODO: 어드민 권한이 있는 사람만 역할을 추가할 수 있도록 수정
+
+  public ResponseEntity<ResultResponse> addRole(@RequestBody RoleRegistrationRequest roleRegistrationRequest) {
+    roleService.saveRole(roleRegistrationRequest);
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.ROLE_REGISTRATION_SUCCESS));
   }
 
   @PostMapping("/delete")
-  public String deleteRole(@RequestBody RoleDeleteRequest roleDeleteRequest) {
-    if(roleService.deleteRole(roleDeleteRequest)) {
-      return "역할 삭제 완료";
-    } else {
-      return "역할 삭제 실패";
-    }
+  public ResponseEntity<ResultResponse> deleteRole(@RequestBody RoleDeleteRequest roleDeleteRequest) {
+    // TODO: 어드민 권한이 있는 사람만 역할을 삭제할 수 있도록 수정
+    roleService.deleteRole(roleDeleteRequest);
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.ROLE_DELETE_SUCCESS));
   }
 
   @GetMapping("/list")
-  public List<Role> getRoleList() {
-    return roleService.findAll();
+  public ResponseEntity<ResultResponse> getRoleList() {
+    // TODO: 로그인된 회원만 역할 목록을 볼 수 있도록 수정
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.ROLE_LIST_REQUEST_SUCCESS, roleService.findAll()));
+  }
+
+  @PostMapping("/update")
+  public ResponseEntity<ResultResponse> updateRole(@RequestBody RoleUpdateRequest roleUpdateRequest) {
+    roleService.updateRole(roleUpdateRequest);
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.ROLE_UPDATE_SUCCESS));
   }
 }
