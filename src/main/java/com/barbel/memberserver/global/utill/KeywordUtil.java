3d94d9db334592b8a0938141e998e21d1acafd6e
WooJoongKim <rlandwnd555@naver.com>
2023-03-05 23:39:30 +0900

    fix : 키워드 등록 DTO 생성자 파라미터 수정에 따른 유틸 코드 수정

--- src/main/java/com/barbel/memberserver/global/utill/KeywordUtil.java
+++ src/main/java/com/barbel/memberserver/global/utill/KeywordUtil.java
@@ -6,7 +6,7 @@ import com.barbel.memberserver.domain.keyword.dto.KeywordRegistrationRequest;
 public class KeywordUtil {
   public static Keyword keywordRegistrationRequestToKeyword(KeywordRegistrationRequest keywordRegistrationRequest) {
     return Keyword.builder()
-        .keyword(keywordRegistrationRequest.getKeyword())
+        .word(keywordRegistrationRequest.getKeyword())
         .build();
   }
 }
