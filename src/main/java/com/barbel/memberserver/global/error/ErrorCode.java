513059f03db16e77abdfc75985cbd058abfddf1e
WooJoongKim <rlandwnd555@naver.com>
2023-03-05 22:12:54 +0900

    feat : 에러 코드 추가

--- src/main/java/com/barbel/memberserver/global/error/ErrorCode.java
+++ src/main/java/com/barbel/memberserver/global/error/ErrorCode.java
@@ -7,26 +7,35 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+  // Global
+  INVALID_INPUT_VALUE(400, "G001", "유효하지 않은 입력값입니다."),
+  INTERNAL_SERVER_ERROR(500, "G002", "서버 내부 오류입니다."),
+  FILE_CONVERT_ERROR(500, "G003", "파일 변환 오류입니다."),
+  UN_AUTHORIZED_ACCESS(401, "G004", "권한이 없습니다."),
+
   // Member
-  MEMBER_NOT_FOUND("M001", "사용자를 찾을 수 없습니다."),
-  MEMBER_ACCOUNT_DUPLICATED("M002", "회원 아이디 중복"),
-  MEMBER_ACCOUNT_NOT_DUPLICATED("M003", "회원 아이디 중복되지않음"),
-  MEMBER_LOGIN_SUCCESS("M004", "회원 로그인 성공"),
-  MEMBER_LOGOUT_SUCCESS("M005", "회원 로그아웃 성공"),
-  MEMBER_UPDATE_SUCCESS("M006", "회원 프로필 업데이트 성공"),
+  MEMBER_NOT_FOUND(400, "M001", "사용자를 찾을 수 없습니다."),
+  MEMBER_ACCOUNT_DUPLICATED(400, "M002", "회원 아이디 중복"),
+  MEMBER_UPDATE_FAIL(400, "M003", "회원 프로필 업데이트 실패"),
+  MEMBER_LIST_REQUEST_FAIL(500, "M004", "회원 리스트 요청 실패"),
 
   // Role
-  ROLE_NOT_FOUND("R001", "권한을 찾을 수 없습니다."),
-  ROLE_REGISTRATION_FAIL("R002", "권한 등록 실패"),
-  ROLE_UPDATE_FAIL("R003", "권한 수정 실패"),
-  ROLE_DELETE_FAIL("R004", "권한 삭제 실패"),
+  ROLE_NOT_FOUND(400, "R001", "역할을 찾을 수 없습니다."),
+  ROLE_LIST_REQUEST_FAIL(500, "R006", "역할 리스트 요청 실패"),
+  ROLE_REGISTRATION_FAIL(500, "R002", "역할 등록 실패"),
+  ROLE_UPDATE_FAIL(500, "R003", "역할 수정 실패"),
+  ROLE_DELETE_FAIL(500, "R004", "역할 삭제 실패"),
+  ROLE_DUPLICATED(400, "R005", "이미 등록되어있는 역할입니다."),
 
   // keyword
-  KEYWORD_NOT_FOUND("K001", "키워드를 찾을 수 없습니다."),
-  KEYWORD_REGISTRATION_FAIL("K002", "키워드 등록 실패"),
-  KEYWORD_UPDATE_FAIL("K003", "키워드 수정 실패"),
-  KEYWORD_DELETE_FAIL("K004", "키워드 삭제 실패");
+  KEYWORD_NOT_FOUND(400, "K001", "키워드를 찾을 수 없습니다."),
+  KEYWORD_REGISTRATION_FAIL(500, "K002", "키워드 등록 실패"),
+  KEYWORD_UPDATE_FAIL(500, "K003", "키워드 수정 실패"),
+  KEYWORD_DELETE_FAIL(500, "K004", "키워드 삭제 실패"),
+  KEYWORD_DUPLICATED(400, "K005", "이미 등록되어있는 키워드입니다."),
+  KEYWORD_LIST_REQUEST_FAIL(500, "K006", "키워드 리스트 요청 실패");
 
+  private final int status;
   private final String code;
   private final String message;
 
