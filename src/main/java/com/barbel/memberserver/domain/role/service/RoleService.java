ef0e8ea7267bb83b545dde96321984bb968d68bd
WooJoongKim <rlandwnd555@naver.com>
2023-03-05 23:43:13 +0900

    feat : 업데이트 메서드 추가 및 실패 시 예외 발생시키도록 로직 대거 수정

--- src/main/java/com/barbel/memberserver/domain/role/service/RoleService.java
+++ src/main/java/com/barbel/memberserver/domain/role/service/RoleService.java
@@ -3,7 +3,13 @@ package com.barbel.memberserver.domain.role.service;
 import com.barbel.memberserver.domain.role.document.Role;
 import com.barbel.memberserver.domain.role.dto.RoleDeleteRequest;
 import com.barbel.memberserver.domain.role.dto.RoleRegistrationRequest;
+import com.barbel.memberserver.domain.role.dto.RoleUpdateRequest;
+import com.barbel.memberserver.domain.role.exception.RoleDeleteFailedException;
+import com.barbel.memberserver.domain.role.exception.RoleDuplicatedException;
+import com.barbel.memberserver.domain.role.exception.RoleListRequestFailedException;
+import com.barbel.memberserver.domain.role.exception.RoleUpdateFailedException;
 import com.barbel.memberserver.domain.role.repository.RoleRepository;
+import com.barbel.memberserver.global.error.exception.BusinessException;
 import com.barbel.memberserver.global.utill.RoleUtil;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
@@ -16,29 +22,52 @@ import java.util.List;
 public class RoleService {
   private final RoleRepository roleRepository;
 
-  public boolean saveRole(RoleRegistrationRequest roleRegistrationRequest) {
-    Role role = RoleUtil.roleRegistrationRequestToRole(roleRegistrationRequest);
-
-    try{
-      roleRepository.save(role);
-      return true;
-    } catch (Exception e) {
-      return false;
+  public void saveRole(RoleRegistrationRequest roleRegistrationRequest) {
+    if(isDuplicateRole(roleRegistrationRequest.getName())) {
+      throw new RoleDuplicatedException();
     }
+    Role role = RoleUtil.roleRegistrationRequestToRole(roleRegistrationRequest);
+    roleRepository.save(role);
   }
 
-  public boolean deleteRole(RoleDeleteRequest roleDeleteRequest) {
-    String roleName = roleDeleteRequest.getRole();
-    try{
-      roleRepository.deleteByName(roleName);
-      return true;
-    } catch (Exception e) {
-      return false;
+  public void deleteRole(RoleDeleteRequest roleDeleteRequest) {
+    if(!isDuplicateRole(roleDeleteRequest.getRole())) {
+      throw new RoleDeleteFailedException();
     }
-
+    roleRepository.deleteById(roleDeleteRequest.getRole());
   }
 
   public List<Role> findAll() {
-    return roleRepository.findAll();
+    try{
+      return roleRepository.findAll();
+    } catch (Exception e) {
+      throw new RoleListRequestFailedException();
+    }
+  }
+
+  public void updateRole(RoleUpdateRequest roleUpdateRequest) {
+    if(roleUpdateRequest.getRolePre().equals(roleUpdateRequest.getRolePost())) {
+      Role role = roleRepository.findById(roleUpdateRequest.getRolePre()).orElseThrow(
+          () -> new RoleUpdateFailedException()
+      );
+      role.setDescription(roleUpdateRequest.getDescription());
+      roleRepository.save(role);
+    } else {
+      roleRepository.deleteById(roleUpdateRequest.getRolePre());
+      try{
+        deleteRole(RoleDeleteRequest.builder()
+            .role(roleUpdateRequest.getRolePre()).build());
+
+        saveRole(RoleRegistrationRequest.builder()
+            .name(roleUpdateRequest.getRolePost())
+            .description(roleUpdateRequest.getDescription()).build());
+      } catch (BusinessException e) {
+        throw new RoleUpdateFailedException();
+      }
+    }
+  }
+
+  public boolean isDuplicateRole(String roleName) {
+    return roleRepository.existsById(roleName);
   }
 }
