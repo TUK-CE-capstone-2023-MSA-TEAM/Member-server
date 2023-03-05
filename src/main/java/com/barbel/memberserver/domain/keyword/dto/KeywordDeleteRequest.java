cda49d24dfc968ecb20be0e7b44cb4e7a7a1ac63
WooJoongKim <rlandwnd555@naver.com>
2023-03-05 23:37:31 +0900

    fix : 키워드 삭제 DTO 애노테이션 수정

--- src/main/java/com/barbel/memberserver/domain/keyword/dto/KeywordDeleteRequest.java
+++ src/main/java/com/barbel/memberserver/domain/keyword/dto/KeywordDeleteRequest.java
@@ -1,8 +1,18 @@
 package com.barbel.memberserver.domain.keyword.dto;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@Data
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class KeywordDeleteRequest {
   private String keyword;
 }
