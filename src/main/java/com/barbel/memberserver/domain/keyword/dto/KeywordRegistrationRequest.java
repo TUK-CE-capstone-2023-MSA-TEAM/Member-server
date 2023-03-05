bc4b42b07b4eb9948b6699acec7553b610051f25
WooJoongKim <rlandwnd555@naver.com>
2023-03-05 23:37:50 +0900

    fix : 키워드 등록 DTO 애노테이션 수정

--- src/main/java/com/barbel/memberserver/domain/keyword/dto/KeywordRegistrationRequest.java
+++ src/main/java/com/barbel/memberserver/domain/keyword/dto/KeywordRegistrationRequest.java
@@ -2,10 +2,17 @@ package com.barbel.memberserver.domain.keyword.dto;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@Data
+@Getter
+@Builder
+@ToString
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class KeywordRegistrationRequest {
   private String keyword;
 }
