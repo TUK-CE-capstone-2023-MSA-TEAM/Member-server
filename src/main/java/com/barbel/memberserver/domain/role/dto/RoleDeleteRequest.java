f6a36b5016c7b26dfafedd851763b5f0c4702ee9
WooJoongKim <rlandwnd555@naver.com>
2023-03-05 23:42:01 +0900

    fix : 애노테이션 수정

--- src/main/java/com/barbel/memberserver/domain/role/dto/RoleDeleteRequest.java
+++ src/main/java/com/barbel/memberserver/domain/role/dto/RoleDeleteRequest.java
@@ -1,8 +1,17 @@
 package com.barbel.memberserver.domain.role.dto;
 
-import lombok.Data;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-@Data
+@Getter
+@Builder
+@ToString
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RoleDeleteRequest {
   private String role;
 }
