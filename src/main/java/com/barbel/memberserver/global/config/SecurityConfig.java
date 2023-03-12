8995713d28f731197934baea067f0f314c9f02e7
WooJoongKim <rlandwnd555@naver.com>
2023-03-12 23:32:52 +0900

    feat : SecurityConfig 설정 클래스 추가

---
+++ src/main/java/com/barbel/memberserver/global/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.barbel.memberserver.global.config;
+
+import com.barbel.memberserver.global.jwt.Provider.JwtTokenProvider;
+import com.barbel.memberserver.global.jwt.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+  private final JwtTokenProvider jwtTokenProvider;
+
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.httpBasic().disable()
+        .csrf().disable()
+        .sessionManagement()
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .authorizeRequests()
+        .antMatchers("/api/member/signup", "/api/member/signin", "/swagger-ui/**").permitAll()
+        .antMatchers("/v2/api-docs",  "/configuration/ui",
+            "/swagger-resources", "/configuration/security",
+            "/swagger-ui.html", "/webjars/**","/swagger/**").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+
+    return http.build();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
+}
