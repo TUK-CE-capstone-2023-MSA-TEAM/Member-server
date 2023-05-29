c4ae234cf2343ccc70a1c8c1b29db91c6564af5d
Identityless <rlandwnd555@naver.com>
2023-05-29 17:58:50 +0900

    feat : 회원 정보 업데이트 서비스 로직 구현
    - 회원 정보 수정
    - 회원 패스워드 수정
    - 회원 프로필 이미지 수정

--- src/main/java/com/barbel/memberserver/domain/member/service/MemberUpdateService.java
+++ src/main/java/com/barbel/memberserver/domain/member/service/MemberUpdateService.java
@@ -1,14 +1,65 @@
 package com.barbel.memberserver.domain.member.service;
 
+import com.barbel.memberserver.domain.member.document.Member;
+import com.barbel.memberserver.domain.member.dto.MemberPasswordUpdateRequest;
+import com.barbel.memberserver.domain.member.dto.MemberUpdateRequest;
+import com.barbel.memberserver.domain.member.exception.MemberNotFountException;
+import com.barbel.memberserver.domain.member.exception.WrongPasswordException;
 import com.barbel.memberserver.domain.member.repository.MemberRepository;
+import com.barbel.memberserver.global.aws.S3Uploader;
+import com.barbel.memberserver.global.utill.MemberUtil;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberUpdateService {
   private final MemberRepository memberRepository;
+  private final S3Uploader s3Uploader;
+  private final PasswordEncoder passwordEncoder;
+  private final String S3_DIR_NAME = "/member/profileImage/";
 
-  // TODO: MemberUpdateService 구현
+  @Transactional
+  public void updateMemberCommon(
+      MemberUpdateRequest memberUpdateRequest
+  ) {
+    Member member = memberRepository.findById(memberUpdateRequest.getMemberId())
+        .orElseThrow(MemberNotFountException::new);
+    MemberUtil.updateMember(memberUpdateRequest, member);
+    memberRepository.save(member);
+  }
+
+  @Transactional
+  public void updateMemberPassword(
+      MemberPasswordUpdateRequest memberPasswordUpdateRequest
+  ) {
+    Member member = memberRepository.findById(memberPasswordUpdateRequest.getMemberId())
+        .orElseThrow(MemberNotFountException::new);
+    if(!passwordEncoder.encode(member.getPassword()).equals(memberPasswordUpdateRequest.getPassword())) {
+      throw new WrongPasswordException();
+    }
+    member.setPassword(memberPasswordUpdateRequest.getNewPassword());
+    memberRepository.save(member);
+  }
+
+  @Transactional
+  public void updateMemberProfileImage(
+      String memberId,
+      MultipartFile profileImage
+  ) {
+    Member member = memberRepository.findById(memberId)
+        .orElseThrow(MemberNotFountException::new);
+    member.setProfileImageURL(uploadProfileImage(profileImage));
+    memberRepository.save(member);
+  }
+
+  private String uploadProfileImage(
+      MultipartFile profileImage
+  ) {
+    return s3Uploader.uploadImage(profileImage, S3_DIR_NAME);
+  }
 }
