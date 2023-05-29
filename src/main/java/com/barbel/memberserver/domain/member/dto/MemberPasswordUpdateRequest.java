6cb7d2a34c8abe16cea1cbcaac01c8db82c328e0
Identityless <rlandwnd555@naver.com>
2023-05-29 17:44:23 +0900

    fix : 회원 패스워드 업데이트 요청 DTO에 memberId 필드 추가

--- src/main/java/com/barbel/memberserver/domain/member/dto/MemberPasswordUpdateRequest.java
+++ src/main/java/com/barbel/memberserver/domain/member/dto/MemberPasswordUpdateRequest.java
@@ -7,6 +7,7 @@ import lombok.*;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberPasswordUpdateRequest {
+    private String memberId;
     private String password;
     private String newPassword;
 
