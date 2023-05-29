b2c28121d2dfdc102bb3ac893eac164984644c25
Identityless <rlandwnd555@naver.com>
2023-05-29 17:58:07 +0900

    fix : memberId 필드 추가

--- src/main/java/com/barbel/memberserver/domain/member/dto/MemberUpdateRequest.java
+++ src/main/java/com/barbel/memberserver/domain/member/dto/MemberUpdateRequest.java
@@ -1,7 +1,6 @@
 package com.barbel.memberserver.domain.member.dto;
 
 import lombok.*;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -10,6 +9,7 @@ import java.util.List;
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MemberUpdateRequest {
+  private String memberId;
   private String nickname;
   private String address;
   private String addressDetail;
