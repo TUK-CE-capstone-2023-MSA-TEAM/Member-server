73016b9caff82928a5c9220a42cdb90c520c0f60
Identityless <rlandwnd555@naver.com>
2023-05-29 17:45:06 +0900

    fix : 테스트 코드 구조 수정

--- src/test/java/com/barbel/memberserver/MemberserverApplicationTests.java
+++ src/test/java/com/barbel/memberserver/MemberserverApplicationTests.java
@@ -1,6 +1,7 @@
 package com.barbel.memberserver;
 
 import com.barbel.memberserver.domain.member.document.Member;
+import com.barbel.memberserver.domain.member.dto.MemberRegistrationRequest;
 import com.barbel.memberserver.domain.member.service.LoginService;
 import com.barbel.memberserver.domain.member.service.MemberService;
 import org.junit.jupiter.api.Test;
@@ -23,8 +24,8 @@ class MemberserverApplicationTests {
 
   @Test
   void dbConnectionTest() {
-    Member member = Member.builder().email("rlandwnd555@naver.com")
-        .age(25).name("김우중").password("password").sex("남자").nickname("로니콜먼")
+    MemberRegistrationRequest member = MemberRegistrationRequest.builder().email("rlandwnd555@naver.com")
+        .age(25).name("김우중").password("password").gender("남자").nickname("로니콜먼")
         .address("서울특별시").addressDetail("강남구").role("고수").interests(List.of("운동", "독서")).majors(List.of("컴퓨터공학")).build();
     try {
       loginService.saveMember(member);
