4e5d53f92f23c62632d4c943d4f544d947db64d1
Identityless <rlandwnd555@naver.com>
2023-05-29 17:57:40 +0900

    feat : 프로필 이미지 수정 엔드포인트 추가 및 비밀 번호, 회원 정보 수정 엔드포인트 내부 코드 구현

--- src/main/java/com/barbel/memberserver/domain/member/controller/MemberUpdateController.java
+++ src/main/java/com/barbel/memberserver/domain/member/controller/MemberUpdateController.java
@@ -4,29 +4,46 @@ import static com.barbel.memberserver.domain.member.controller.MemberController.
 
 import com.barbel.memberserver.domain.member.dto.MemberPasswordUpdateRequest;
 import com.barbel.memberserver.domain.member.dto.MemberUpdateRequest;
+import com.barbel.memberserver.domain.member.service.MemberUpdateService;
+import com.barbel.memberserver.global.result.ResultCode;
+import com.barbel.memberserver.global.result.ResultResponse;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 @RequestMapping(MEMBER_API_URI)
 @Slf4j
 public class MemberUpdateController {
+  private final MemberUpdateService memberUpdateService;
 
-  //TODO: 회원 정보 수정 기능 구현.
   @PostMapping("/update/common")
-  public String update(@RequestBody MemberUpdateRequest memberUpdateRequest) {
+  @Operation(summary = "기본 회원정보 수정")
+  public ResponseEntity<ResultResponse> update(@RequestBody MemberUpdateRequest memberUpdateRequest) {
     log.info(memberUpdateRequest.toString());
-    return "업데이트 완료";
+    memberUpdateService.updateMemberCommon(memberUpdateRequest);
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.MEMBER_UPDATE_SUCCESS));
   }
   @PostMapping("/update/password")
-  public String updatePassword(@RequestBody MemberPasswordUpdateRequest memberPasswordUpdateRequest) {
+  @Operation(summary = "비밀번호 수정")
+  public ResponseEntity<ResultResponse> updatePassword(@RequestBody MemberPasswordUpdateRequest memberPasswordUpdateRequest) {
     log.info(memberPasswordUpdateRequest.toString());
-    return "비밀번호 업데이트 완료";
+    memberUpdateService.updateMemberPassword(memberPasswordUpdateRequest);
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.MEMBER_PASSWORD_UPDATE_SUCCESS));
+  }
+
+  @PostMapping("/update/profileImage")
+  @Operation(summary = "프로필 이미지 수정")
+  public ResponseEntity<ResultResponse> updateProfileImage(
+      @RequestPart String memberId,
+      @RequestBody MultipartFile profileImage)
+  {
+    memberUpdateService.updateMemberProfileImage(memberId, profileImage);
+    return ResponseEntity.ok(ResultResponse.of(ResultCode.MEMBER_PROFILE_IMAGE_UPDATE_SUCCESS));
   }
 }
