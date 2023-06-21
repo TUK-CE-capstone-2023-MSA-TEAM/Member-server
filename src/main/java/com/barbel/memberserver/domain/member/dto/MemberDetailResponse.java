94119cc52a35e9e95bd22ecd4616067e5ab2f33f
WooJoong Kim <rlandwnd555@naver.com>
2023-06-21 21:37:20 +0900

    feat : 회원 디테일 응답 dto 추가

---
+++ src/main/java/com/barbel/memberserver/domain/member/dto/MemberDetailResponse.java
@@ -0,0 +1,21 @@
+package com.barbel.memberserver.domain.member.dto;
+
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class MemberDetailResponse {
+  private String email;
+  private String nickname;
+  private int age;
+  private String gender;
+  private String introduce;
+  private String profileImageURL;
+  private String role;
+  private List<String> interests;
+  private List<String> majors;
+}
