c7dedbf50e5185d8d8d437d8c2aebaa9b66fccc6
WooJoong Kim <rlandwnd555@naver.com>
2023-06-21 21:37:40 +0900

    refactor : 클래스 이름 오타 수정

--- src/main/java/com/barbel/memberserver/domain/member/exception/MemberNotFoundException.java
+++ src/main/java/com/barbel/memberserver/domain/member/exception/MemberNotFoundException.java
@@ -3,8 +3,8 @@ package com.barbel.memberserver.domain.member.exception;
 import com.barbel.memberserver.global.error.ErrorCode;
 import com.barbel.memberserver.global.error.exception.BusinessException;
 
-public class MemberNotFountException extends BusinessException {
-  public MemberNotFountException() {
+public class MemberNotFoundException extends BusinessException {
+  public MemberNotFoundException() {
     super(ErrorCode.MEMBER_NOT_FOUND);
   }
 }
